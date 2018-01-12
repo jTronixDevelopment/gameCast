0f3da398df60ae5318b817b7c3c9a5aed6430675
Ryan <johnson2500@live.com>
2018-01-11 21:47:58 -0800

    Setup and Resolving Errors
    Setup and Resolving Errors

--- server/app.js
+++ server/app.js
@@ -4,6 +4,11 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+// app.use(require('express').static('src'));
+// app.get('/', function(req, res) {
+//   res.sendFile("/Users/Ryan/Documents/GitHub/projectX/index.html");
+//   console.log(__dirname)
+// });
 
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
