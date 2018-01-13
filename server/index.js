7520c11c8444853032d7f1b549cb2558aeb94579
Ryan <johnson2500@live.com>
2018-01-13 14:17:09 -0800

    Clean UP
    Cleaninng up

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 // server/index.js
-
 'use strict';
-
 const app = require('./app');
-
 const PORT = process.env.PORT || 9000;
-
 require('./socket/socketServer.js'); // Start Socket Server
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`) );
