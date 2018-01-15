298f61d4756d8bd789b778ee019f95fe2626f801
Ryan <johnson2500@live.com>
2018-01-15 09:47:58 -0800

    Final Working
    Final working build
    With test and Prod Enviornemnts working

--- server/index.js
+++ server/index.js
@@ -1,30 +1,4 @@
-// const express = require('express');
-// const socketIO = require('socket.io');
-// const path = require('path');
-//
-// const PORT = process.env.PORT || 3000;
-// const INDEX = path.resolve(__dirname, '..', 'build', 'index.html')
-//
-// const app = express();
-// // app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
-//
-// // Serve static assets
-// app.use(express.static(path.resolve(__dirname, '..', 'build')));
-//
-// // Always return the main index.html, so react-router render the route in the client
-// app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html')));
-//
-// const io = socketIO(app);
-//
-// io.on('connection', (socket) => {
-//   console.log('Client connected');
-//   socket.on('disconnect', () => console.log('Client disconnected'));
-// });
-//
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-//
-//
-
+//New Server
 const express = require('express');
 var app = express();
 const path = require('path');
@@ -32,38 +6,45 @@ const path = require('path');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 5000);
+server.listen(process.env.PORT || 5000);// For production
 
-console.log(__dirname)
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html')));
 
-io.on('connection', function (socket) {
-  console.log("connection")
-  setTimeout(function(){
-    socket.emit('news', { hello: 'world' });
+//==============================================================================
+//=== Socket IO Stuff ==========================================================
+//==============================================================================
 
-   }, 3000);
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+var ioHandlers = require('./socket/ioHandlers.js'); // Will export all functions from ioHandlers File
+
+console.log(`Socket Server Running on ${process.env.PORT || 5000}!`);
+
+var game1 = io.of('/game1'); // Name Space of game1
+var game2 = io.of('/game2'); // Name Space of game2
+var game3 = io.of('/game3'); // Name Space of game3
+
+io.on('connection',(socket)=>{ // Generic "/" route which everyone hits first
+
+  // Logic for joining room
+  console.log("Connection Made")
+
+  socket.on('joinGame',(msg)=>{
+      if(msg === "game1"){
+
+      } else {
+
+      }
+  })
+  // Logic for starting room
+
+  socket.on('startGame',()=>{
+    console.log(msg);
+  })
+
+  // logic for leaving room
 
 });
 
-
-
-
-
-
-
-
-
-
-// // server/index.js
-// 'use strict';
-// const app = require('./app');
-// const PORT = process.env.PORT || 9000;
-// require('./socket/socketServer.js'); // Start Socket Server
-//
-// app.listen(PORT, () => console.log(`App listening on port ${PORT}!`) );
+game1.on('connection',(socket)=>{ ioHandlers.gameHandler1(socket) })
+game2.on('connection',(socket)=>{ ioHandlers.gameHandler2(socket) })
+game3.on('connection',(socket)=>{ ioHandlers.gameHandler3(socket) })
